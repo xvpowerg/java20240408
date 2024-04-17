69aebd489652c575f4bb84f0eb7788798ab67bf5
xvpowerg <c911010083@gmail.com>
2024-04-17 22:14:25 +0800

    Delete Ch16_module_1 directory

--- Ch16_module_1/src/tw.com.m1/classes/module-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/J2SEModule/module-info.java to edit this template
- */
-
-module tw.com.m1 {
-    exports tw.com.m1.fruit to tw.com.m2;
-    exports tw.com.m1.person to tw.com.m3;
-}
--- Ch16_module_1/src/tw.com.m2/classes/module-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/J2SEModule/module-info.java to edit this template
- */
-
-module tw.com.m2 {
-    requires tw.com.m1;
-}
